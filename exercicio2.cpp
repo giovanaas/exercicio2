e3acb8765bd7405fb4f648acd3ec050beb825f91
giovanaas <azevedogiovana309@gmail.com>
2025-03-31 11:24:52 -0300

    exercicio2

---
+++ exercicio2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int altura1, altura2, altura3;
+
+    cout <<"por favor, insira as alturas dos 3 professores, em cm:  " << endl;
+    cin >> altura1;
+    cin >> altura2;
+    cin >> altura3;
+
+    if (altura1<150 || altura1>220 || altura2<150 || altura2>220 || altura3<150 || altura3>220) {
+        cout << "As alturas que voce inseriu sao invalidas!";
+    }
+    else if (altura1>altura2 && altura1<altura3) {
+        cout <<"A altura do professor Gatti eh " << altura1 << " cm." << endl;
+    } 
+    else if ( altura2>altura1 && altura2<altura3) {
+        cout << "A altura do professor Gatti eh " << altura2 << " cm." << endl;
+    } 
+    else {
+        cout << "A altura do professor Gatti eh " << altura3 << " cm." << endl;
+    }
+
+}
